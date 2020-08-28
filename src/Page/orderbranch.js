4d02b376cd3e69a97447ea6794b8c78fb86cca18
tanawatser <tanawat.sr.58@ubu.ac.th>
2020-08-28 18:03:41 +0700

    update tsx

---
+++ src/Page/orderbranch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class orderbranch extends Component {
+    render() {
+        return (
+            <div>
+                สั่งสินค้า
+            </div>
+        )
+    }
+}
--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Homepage from './Page/homepage'
+import Orderbranch from './Page/orderbranch'
 
 export default class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route exact path='/orderbranch' component={Orderbranch} />
+
 
 
         <Route
